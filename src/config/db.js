6c8f286739d7f1306a1bdc7cda21040e4a32447b
monnishsayireddy peta <monnishreddy@monnishsayireddys-MacBook-Air.local>
2026-04-14 14:54:03 -0400

    Fix Prisma client import for production

--- src/config/db.js
+++ src/config/db.js
@@ -1,7 +1,5 @@
-import { PrismaClient } from '../generated/prisma/client.js';
-import { PrismaPg } from '@prisma/adapter-pg';
+import { PrismaClient } from '@prisma/client';
 
-const adapter = new PrismaPg({ connectionString: process.env.DATABASE_URL });
-const prisma = new PrismaClient({ adapter });
+const prisma = new PrismaClient();
 
 export default prisma;
